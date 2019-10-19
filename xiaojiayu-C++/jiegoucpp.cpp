4ad8a3360c43b7892d016cd78cc2318758205450
Alex- bd <1178276956@qq.com>
2019-10-19 11:26:51 +0800

    指针

---
+++ xiaojiayu-C++/jiegoucpp.cpp
@@ -0,0 +1,20 @@
+/*Ω·ππ
+”Ô∑®£∫
+	struct name
+	{
+	}name;
+*/
+#include<iostream>
+struct FishOil
+{
+	std::string name;
+	std::string uid;
+	char sex;
+};
+
+FishOil jiayu = {"wang","123",'ƒ–'};
+FishOil *point = &jiayu;
+(*point).name = "∫⁄“π";
+point->name = "h";
+
+
